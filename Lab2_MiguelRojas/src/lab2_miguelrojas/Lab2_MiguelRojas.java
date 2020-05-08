63c190bc1c2c4aae2966abf0742b252f5fe7016e
Miguel Rojas <m_rojas18@unitec.edu>
2020-05-08 14:13:39 -0600

    Opcion Crear Casa

--- Lab2_MiguelRojas/src/lab2_miguelrojas/Lab2_MiguelRojas.java
+++ Lab2_MiguelRojas/src/lab2_miguelrojas/Lab2_MiguelRojas.java
@@ -1,5 +1,7 @@
 package lab2_miguelrojas;
 
+import java.security.acl.Owner;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Lab2_MiguelRojas {
@@ -9,6 +11,8 @@ public class Lab2_MiguelRojas {
         Scanner sc = new Scanner(System.in);
         char resp = 's';
         boolean login = false;
+        
+        ArrayList<Casa> lista_casas = new ArrayList();
         while(resp == 's'){
             System.out.print("Menu\n"
                     + "[1] Registro Casas\n"
@@ -26,6 +30,144 @@ public class Lab2_MiguelRojas {
                         System.out.print("Debe ingresar a su cuenta para poder acceder a esta funcion.");
                     } else{
                         //TO DO: Menu Registro de Casas
+                        boolean flag_menu2 = true;
+                        while(flag_menu2){
+                            System.out.print("Menu---Registro Casas\n"
+                                    + "[1] Crear Casas\n"
+                                    + "[2] Listar Casas\n"
+                                    + "[3] Modificar Casas\n"
+                                    + "[4] Borrar Casas\n"
+                                    + "[5] Salir\n"
+                                    + "Seleccione una opcion: ");
+                            int opcion_m2 = sc.nextInt();
+                            sc = new Scanner(System.in);
+                            System.out.println();
+                            switch(opcion_m2){
+                                case 1:
+                                    //Ingreso de Datos para una Casa
+                                    //Numero de Casa
+                                    System.out.print("Crear Casa\n"
+                                            + "Ingrese numero de Casa: ");
+                                    int num_casa = sc.nextInt();
+                                    sc = new Scanner(System.in);
+                                    //Numero de Bloque
+                                    System.out.print("Ingrese numero de bloque: ");
+                                    int num_bloque = sc.nextInt();
+                                    sc = new Scanner(System.in);
+                                    //Color
+                                    System.out.print("Ingrese color de la casa: ");
+                                    String color_casa = sc.next();
+                                    sc = new Scanner(System.in);
+                                    //Ancho
+                                    System.out.print("Ingrese ancho de la casa: ");
+                                    int ancho = sc.nextInt();
+                                    sc = new Scanner(System.in);
+                                    //Largo
+                                    System.out.print("Ingrese largo de la casa: ");
+                                    int largo = sc.nextInt();
+                                    sc = new Scanner(System.in);
+                                    //Comprada
+                                    System.out.print("Casa Comprada\n"
+                                            + "[1] Si\n"
+                                            + "[2] No\n"
+                                            + "Eliga situacion de la casa: ");
+                                    int opcion_compra = sc.nextInt();
+                                    sc = new Scanner(System.in);
+                                    String casa_compra = "";
+                                    if (opcion_compra == 1) {
+                                        casa_compra = "Si";
+                                    } else {
+                                        casa_compra = "No";
+                                    }
+                                    //Numero de Pisos
+                                    System.out.print("Ingrese numero de pisos: ");
+                                    int num_pisos = sc.nextInt();
+                                    sc = new Scanner(System.in);
+                                    //Numero de Banos
+                                    System.out.print("Ingrese numero de baños: ");
+                                    int num_banos = sc.nextInt();
+                                    //Numero de Cuartos
+                                    System.out.print("Ingrese numero de cuartos: ");
+                                    int num_cuartos = sc.nextInt();
+                                    //Dueño
+                                    String owner_casa = "";
+                                    if (casa_compra.equals("Si")) {
+                                        System.out.print("Ingrese nombre del dueño de la casa: ");
+                                        owner_casa = sc.nextLine();
+                                        sc = new Scanner(System.in);
+                                    } else {
+                                        owner_casa = "Sin dueño";
+                                    }
+                                    //Estado
+                                    boolean flag_estado = true;
+                                    String estado_casa= "";
+                                    while (flag_estado) {                                        
+                                        System.out.print("Estado de la Casa\n"
+                                            + "[1] Lista\n"
+                                            + "[2] Construcción\n"
+                                            + "[3] Construcción en Espera\n"
+                                            + "[4] Espera de Demolición\n"
+                                            + "Eliga estado de la casa: ");
+                                        int op_estado = sc.nextInt();
+                                        switch(op_estado){
+                                            case 1:
+                                                //Estado Lista //ArrayList Despues
+                                                estado_casa = "Lista";
+                                                flag_estado = false;
+                                                break;
+                                            case 2:
+                                                //Estado Construccion //ArrayList Despues
+                                                estado_casa = "Construccion";
+                                                flag_estado = false;
+                                                break;
+                                            case 3:
+                                                //Estado Construccion en Espera //ArrayList Despues
+                                                estado_casa = "Construccion en Espera";
+                                                flag_estado = false;
+                                                break;
+                                            case 4:
+                                                //Estado Espera de Demolicion //ArrayList Despues
+                                                estado_casa = "Espera de Demolicion";
+                                                flag_estado = false;
+                                                break;
+                                            default:
+                                                System.out.println("Ingrese una opcion valida.");
+                                        }
+                                    }
+                                    
+                                    //Nombre del Ingeniero
+                                    System.out.print("Ingrese el nombre del ingeniero a cargo de la casa: ");
+                                    String nombre_ing = sc.nextLine();
+                                    sc = new Scanner(System.in);
+                                    //Agregar Casa a Lista
+                                    lista_casas.add(new Casa(num_casa, num_bloque,color_casa, 
+                                            ancho, largo, casa_compra, num_pisos, num_banos, 
+                                            num_cuartos, owner_casa, estado_casa, nombre_ing));
+                                    System.out.println("Se creo la Casa con exito!!");
+                                    break;
+                                case 2:
+                                    String casas = "";
+                                    for (int i = 0; i < lista_casas.size(); i++) {
+                                        Casa c = lista_casas.get(i);
+                                        casas += "[" + i + "] " + "Numero de Casa: " + c.getNumero_casa() 
+                                                + ", Dueño de Casa: " + c.getNombre_dueno() 
+                                                + ",Estado: "+c.getEstado() + "\n";
+                                    }
+                                    
+                                    System.out.print("Listado de Casas\n"
+                                            + casas);
+                                    break;
+                                case 3:
+                                    break;
+                                case 4:
+                                    break;
+                                case 5:
+                                    flag_menu2 = false;
+                                    break;
+                                default:
+                                    System.out.println("Ingrese una opcion valida.");
+                            }
+                        }
                     }
                     
                     break;
@@ -49,7 +191,7 @@ public class Lab2_MiguelRojas {
                         sc = new Scanner(System.in);
                         if (pw.equals("99")) {
                             System.out.println("Bienvenido Leonardo!!\n"
-                                    + "Ya tiene acceso a las funciones del Programa.");
+                                    + "Ya tiene acceso a las funciones del Programa.\n");
                             login = true;
                         } else {
                             System.out.println("La contraseña es incorrecta.\n");
