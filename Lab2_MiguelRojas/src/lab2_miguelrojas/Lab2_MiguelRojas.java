d26ba6b7f5ddbf87cc1671c3aef2b2a695415411
Miguel Rojas <m_rojas18@unitec.edu>
2020-05-08 13:15:23 -0600

    Primer Commit Inicio de Laboratorio

---
+++ Lab2_MiguelRojas/src/lab2_miguelrojas/Lab2_MiguelRojas.java
@@ -0,0 +1,41 @@
+package lab2_miguelrojas;
+
+import java.util.Scanner;
+
+public class Lab2_MiguelRojas {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        char resp = 's';
+        boolean login = false;
+        while(resp == 's'){
+            System.out.print("Menu\n"
+                    + "[1] Registro Casas\n"
+                    + "[2] Manejo de Estados\n"
+                    + "[3] Log In\n"
+                    + "[4] Salir\n"
+                    + "Seleccione una opcion: ");
+            int opcion = sc.nextInt();
+            sc = new Scanner(System.in);
+            System.out.println();
+            switch(opcion){
+                case 1:
+                    System.out.print("Ingrese su usuario: ");
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    resp = 'n';
+                    break;
+                default:
+                    System.out.println("Ingrese una opcion valida.");
+            }
+        }
+    }
+    
+}
